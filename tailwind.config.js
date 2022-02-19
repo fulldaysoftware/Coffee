921c885960db52abac85b7e0e9b90b45f3c79bf1
fulldaysoftware <mulukennegash18@gmail.com>
2022-02-19 16:25:39 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'cfbg': '#700909',
+      'hltText': '#FFFFFF',
+      'bd-txt': '#2E0909'
+    },
+    fontFamily: {
+      'titleText':['Lobster', 'cursive'],
+      'bdText': ['Roboto', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
